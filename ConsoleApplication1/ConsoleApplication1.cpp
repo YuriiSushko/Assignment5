40ecc9ec4438630b5a9c96e09f4a20915d120673
yurii_sushko <144662854+YuriiSushko@users.noreply.github.com>
2024-07-16 10:50:45 +0300

    shunting yard is complete

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
 #include <stack>
+#include <queue>
 #include <string>
 
 using namespace std;
 
-class Tokenizator
+class Calculator
 {
 private:
-	string* token_list;  
+	string* token_list;
 
 public:
-	Tokenizator() : token_list(nullptr) {}
+	Calculator() : token_list(nullptr) {}
 
-	~Tokenizator() {
+	~Calculator() {
 		delete[] token_list; 
+	}
+
+	int priorities(const string& op) {
+		if (op == "+" || op == "-")
+			return 1;
+		else if (op == "*" || op == "/")
+			return 2;
+		else if (op == "min" || op == "max")
+			return 3;
+		else if (op == "pow" || op == "abs")
+			return 4;
+		else
+			return 0;
 	}
 
 	string* Tokenize(const string& input) {
@@ -32,7 +46,7 @@ public:
 			if (isdigit(input[pos])) {
 				size_t start = pos;
 
-				while (pos < input.length() && (isdigit(input[pos]) || input[pos] == ',')) {
+				while (pos < input.length() && (isdigit(input[pos]) || input[pos] == '.')) {
 					pos++;
 				}
 
@@ -53,14 +67,70 @@ public:
 				continue;
 			}
 
-			if (input[pos] != ',') {
-				token_list[iterator] = input[pos];
+			if (input[pos] == '+' || input[pos] == '-' || input[pos] == '*' || input[pos] == '/'
+				|| input[pos] == '(' || input[pos] == ')' || input[pos] == ',') {
+				token_list[iterator] = input.substr(pos, 1);
 				iterator++;
+				pos++;
+				continue;
 			}
 
 			pos++;
 		}
 		return token_list;
+	}
+
+	queue<string> ShuntingYard(string* tokens) {
+		stack<string> token_stack;
+		queue<string> result;
+		int iterator = 0;
+
+		while (!tokens[iterator].empty()) {
+			if (isdigit(tokens[iterator][0])) {
+				result.push(tokens[iterator]);
+			}
+			else if (!isdigit(tokens[iterator][0]) && tokens[iterator] != "(" && tokens[iterator] != ")" && tokens[iterator] != ",")
+			{
+				while (!token_stack.empty() &&
+					(priorities(token_stack.top()) >= priorities(tokens[iterator]))) 
+				{
+					result.push(token_stack.top());
+					token_stack.pop();
+				}
+				token_stack.push(tokens[iterator]);
+			}
+			else if (tokens[iterator] == "(") 
+			{
+				token_stack.push(tokens[iterator]);
+			}
+			else if (tokens[iterator] == ")") {
+				while (!token_stack.empty() && token_stack.top() != "(") 
+				{
+					result.push(token_stack.top());
+					token_stack.pop();
+				}
+				if (!token_stack.empty()) 
+				{
+					token_stack.pop(); 
+				}
+			}
+			else if (tokens[iterator] == ",") {
+				while (!token_stack.empty() && token_stack.top() != "(")
+				{
+					result.push(token_stack.top());
+					token_stack.pop();
+				}
+				
+			}
+			iterator++;
+		}
+
+		while (!token_stack.empty()) {
+			result.push(token_stack.top());
+			token_stack.pop();
+		}
+
+		return result;
 	}
 };
 
@@ -70,8 +140,9 @@ int main()
 	string input;
 	getline(cin, input); 
 
-	Tokenizator tokenizator = Tokenizator();
-	string* tokens = tokenizator.Tokenize(input);
+	Calculator calculator;
+	string* tokens = calculator.Tokenize(input);
+
 
 	for (int i = 0; i < input.length(); i++) {
 		if (tokens[i].empty()) {
@@ -79,6 +150,16 @@ int main()
 		}
 		cout << "Token: " << tokens[i] << endl;
 	}
+
+	queue<string> sy = calculator.ShuntingYard(tokens);
+
+	while (!sy.empty()) {
+		cout << sy.front() << ", ";
+		sy.pop();
+	}
+	cout << endl;
+
+	delete[] tokens;
 	
 	return 0;
 }
